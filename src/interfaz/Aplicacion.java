44f7bb749a7576990b92ae143f8e7b0495e8d6a5
jngm8 <jn.gomezm1@uniandes.edu.co>
2021-10-10 11:44:10 -0500

    Avance

---
+++ src/interfaz/Aplicacion.java
@@ -0,0 +1,10 @@
+package interfaz;
+
+public class Aplicacion 
+
+{
+	 public static void main(String[] args) 
+	 {
+		
+	 }
+}
--- src/modelo/Cajero.java
+++ src/modelo/Cajero.java
@@ -1,4 +1,4 @@
-package interfaz;
+package modelo;
 
 public class Cajero {
 
