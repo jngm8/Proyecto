3708e8ef6a19ddd257037bc80e6b7ff83e32efa5
jngm8 <jn.gomezm1@uniandes.edu.co>
2021-10-11 09:50:07 -0500

    l

--- src/modelo/Inventario.java
+++ src/modelo/Inventario.java
@@ -7,9 +7,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+
 public class Inventario 
 {
 	private HashMap<Integer, Producto> Productos;
+	
+	private HashMap<String, ArrayList<Integer>> Categorias;
+	
+	public Inventario()
+	{
+		Productos = new HashMap<Integer,Producto>();
+		Categorias = new HashMap<String,ArrayList<Integer>>();
+	}
+	
 	
 
 }
