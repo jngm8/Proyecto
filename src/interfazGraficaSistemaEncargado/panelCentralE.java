6053f990fa80badd78eed4f6b6d3b7187ba41918
jngm8 <jn.gomezm1@uniandes.edu.co>
2021-11-07 22:16:23 -0500

    avance 4

--- src/interfazGraficaSistemaEncargado/panelCentralE.java
+++ src/interfazGraficaSistemaEncargado/panelCentralE.java
@@ -1,5 +1,29 @@
 package interfazGraficaSistemaEncargado;
 
-public class panelCentralE {
+import java.awt.BorderLayout;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class panelCentralE extends JPanel
+
+{
+	
+	private JLabel lblMercado;
+
+	
+	public panelCentralE()
+	{
+		setLayout(new BorderLayout());
+		
+		lblMercado = new JLabel();
+		ImageIcon imagen = new ImageIcon("./data/superEncargado.jpg");
+		lblMercado.setIcon(imagen);
+		add(lblMercado);
+		
+		
+		
+	}
 }
