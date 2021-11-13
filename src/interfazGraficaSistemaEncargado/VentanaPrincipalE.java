eb76e0e45b499dafaa6d30224af2df2c8b13e5a6
jngm8 <jn.gomezm1@uniandes.edu.co>
2021-11-13 18:20:23 -0500

    avance

--- src/interfazGraficaSistemaEncargado/VentanaPrincipalE.java
+++ src/interfazGraficaSistemaEncargado/VentanaPrincipalE.java
@@ -3,6 +3,7 @@ package interfazGraficaSistemaEncargado;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,6 +20,7 @@ import controlador.PersistenciaException;
 import intefrazGraficaSistemaPOS.ImagenPOS;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 public class VentanaPrincipalE extends JFrame
 {
@@ -32,9 +34,17 @@ public class VentanaPrincipalE extends JFrame
 	
 	private JPanel panelInfo;
 	
+	private JPanel panelEliminar;
+	
 	// Importa la lógica del programa
 	
 	private Inventario modelo;	
+	
+	// Declaro botones de eliminar un lote
+	
+	private JButton btnPorID;
+	
+	private JButton btnVencido;
 
 	
 	public VentanaPrincipalE()
@@ -108,6 +118,24 @@ public class VentanaPrincipalE extends JFrame
 	
 	public void EliminarLote()
 	{
+		panelEliminar = new JPanel();
+
+		JDialog dialogo = new JDialog();
+		dialogo.setSize(300,300);
+		dialogo.setLocationRelativeTo(this);
+		dialogo.add(panelEliminar);
+		dialogo.setVisible(true);
+		
+		btnPorID = new JButton("ELIMINAR LOTE POR ID");
+		btnPorID.setBackground(new java.awt.Color(143,171,237));
+		btnPorID.setForeground(Color.BLACK);
+		panelEliminar.add(btnPorID);
+		
+		
+		btnVencido = new JButton("ELIMINAR LOTES VENCIDOS");
+		btnVencido.setBackground(new java.awt.Color(143,171,237));
+		btnVencido.setForeground(Color.BLACK);
+		panelEliminar.add(btnVencido);
 		
 	}
 	
