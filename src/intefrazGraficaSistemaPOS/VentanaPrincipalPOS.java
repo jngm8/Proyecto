6604205effe320dfe78dd47759b300fd3a6832ce
jngm8 <jn.gomezm1@uniandes.edu.co>
2021-11-13 10:50:38 -0500

    rr

--- src/intefrazGraficaSistemaPOS/VentanaPrincipalPOS.java
+++ src/intefrazGraficaSistemaPOS/VentanaPrincipalPOS.java
@@ -114,7 +114,6 @@ public class VentanaPrincipalPOS extends JFrame
 		{
 			JOptionPane.showMessageDialog(this,"\nEl cliente con numero de cedula " + cedula + " no fue encontrado\n");
 		}
-
 	}
 	
 	
