45db7de16c4ae32080b3e9cb45fe7db3dfb0150b
DanielOsorio01 <d.osorioc2@uniandes.edu.co>
2021-12-10 21:40:05 -0500

    dfa

--- src/controlador/Inventario.java
+++ src/controlador/Inventario.java
@@ -299,11 +299,14 @@ public class Inventario
 			Regalo nuevoRegalo = new Regalo(codigoDeBarras, pague, lleva, fechaInicio, fechaVencimiento);
 			Regalos.put(codigoDeBarras, nuevoRegalo);
 		}
+<<<<<<< Updated upstream
 	}
 	
 	public Collection<Descuento> valoresDescuentos()
 	{
 		return Descuentos.values();
+=======
+>>>>>>> Stashed changes
 	}
 
 	/* 
